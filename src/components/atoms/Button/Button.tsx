7b5958385ea282421f08329a73ebc632ab6ee8bb
Aman Dwivedi <dwivediarjun90@gmail.com>
2025-12-17 11:51:07 +0530

    refactor App component structure and add Button component with Tailwind CSS integration

---
+++ src/components/atoms/Button/Button.tsx
@@ -0,0 +1,9 @@
+const Button = () =>  {
+    return(
+
+        <button name="button" className="btn btn-primary btn-outline btn-square btn-lg">click me</button>
+
+    )
+}
+
+export default Button
--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -59,3 +59,5 @@ export const Header = () => {
         </>
     );
 };
+
+export default Header
